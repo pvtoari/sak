5d32b88f21e731eb4fe3624a67c0cf58da9ae473
Ariel Roque <arielroque@protonmail.com>
2025-07-01 16:39:22 +0200

    Binary output file name renamed

--- bare-bones/link-kernel.sh
+++ bare-bones/link-kernel.sh
@@ -1,1 +1,1 @@
-i686-elf-gcc -ffreestanding -nostdlib -m32 -T linker.ld -o kernel.bin boot.o kernel.o
+i686-elf-gcc -ffreestanding -nostdlib -m32 -T linker.ld -o sak.bin boot.o kernel.o
--- bare-bones/qemu.sh
+++ bare-bones/qemu.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -kernel kernel.bin
+qemu-system-i386 -kernel sak.bin
