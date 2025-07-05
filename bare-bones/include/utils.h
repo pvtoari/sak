9dc0d29e09597999f4ca14ba787dcacac69c6afa
Ariel Roque <arielroque@protonmail.com>
2025-07-05 18:00:49 +0200

    Convert function added

--- bare-bones/include/utils.h
+++ bare-bones/include/utils.h
@@ -48,4 +48,23 @@ void itoa(uint32_t num, char* buffer, int buffer_size) {
     
     buffer[i] = '\0';
 }
+
+char *convert(unsigned int num, int base)
+{
+    static char Representation[]= "0123456789ABCDEF";
+    static char buffer[50];
+    char *ptr;
+
+    ptr = &buffer[49];
+    *ptr = '\0';
+
+    do
+    {
+        *--ptr = Representation[num%base];
+        num /= base;
+    }while(num != 0);
+
+    return(ptr);
+}
+
 #endif // UTILS_H
