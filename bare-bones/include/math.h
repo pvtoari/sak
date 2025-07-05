66dd6a55e2bf6d5ddf36677dc0458eac7108f889
Ariel Roque <arielroque@protonmail.com>
2025-07-05 17:53:56 +0200

    Created math header

---
+++ bare-bones/include/math.h
@@ -0,0 +1,12 @@
+#ifndef MATH_H
+#define MATH_H
+
+static inline int min(int a, int b) {
+    return (b < a) ? b : a;
+}
+
+static inline int max(int a, int b) {
+    return (b < a) ? a : b;
+}
+
+#endif
