8b5c16f9a9ce2f62fa3d89aec41103371a27ea20
Ariel Roque <arielroque@protonmail.com>
2025-07-05 17:56:19 +0200

    Sample colors header

---
+++ bare-bones/include/colors.h
@@ -0,0 +1,30 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define C_BLACK        0x00000000
+#define C_WHITE        0x00FFFFFF
+#define C_RED          0x00FF0000
+#define C_LIME         0x0000FF00
+#define C_BLUE         0x000000FF
+#define C_YELLOW       0x00FFFF00
+#define C_CYAN         0x0000FFFF
+#define C_MAGENTA      0x00FF00FF
+#define C_SILVER       0x00C0C0C0
+#define C_GRAY         0x00808080
+#define C_MAROON       0x00800000
+#define C_OLIVE        0x00808000
+#define C_GREEN        0x00008000
+#define C_PURPLE       0x00800080
+#define C_TEAL         0x00008080
+#define C_NAVY         0x00000080
+
+#define C_BROWN        0x00A52A2A
+#define C_ORANGE       0x00FFA500
+#define C_GOLD         0x00FFD700
+#define C_PINK         0x00FFC0CB
+#define C_VIOLET       0x008A2BE2
+#define C_DARK_GREEN   0x00640000
+#define C_DARK_RED     0x008B0000
+#define C_DARK_BLUE    0x0000008B
+
+#endif // COLORS_H
