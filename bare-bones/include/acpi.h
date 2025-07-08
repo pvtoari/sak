8a7faf7914a4a7331b4c825da767e8d358ac8993
Ariel Roque <arielroque@protonmail.com>
2025-07-08 11:19:15 +0200

    ACPI implementation

---
+++ bare-bones/include/acpi.h
@@ -0,0 +1,135 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#ifndef ACPI_H
+#define ACPI_H
+
+typedef struct RSDP_t {
+ char signature[8];
+ uint8_t checksum;
+ char oem_id[6];
+ uint8_t revision;
+ uint32_t rsdt_address;
+} __attribute__ ((packed)) RSDP_t;
+
+typedef struct XSDP_t {
+    char signature[8];
+    uint8_t checksum;
+    char oem_id[6];
+    uint8_t revision;
+    uint32_t rsdt_address;      // deprecated since version 2.0
+
+    uint32_t length;
+    uint64_t xsdt_address;
+    uint8_t extended_checksum;
+    uint8_t reserved[3];
+} __attribute__ ((packed)) XSDP_t;
+
+typedef struct ACPISDTHeader_t {
+  char Signature[4];
+  uint32_t Length;
+  uint8_t Revision;
+  uint8_t Checksum;
+  char OEMID[6];
+  char OEMTableID[8];
+  uint32_t OEMRevision;
+  uint32_t CreatorID;
+  uint32_t CreatorRevision;
+} ACPISDTHeader_t;
+
+typedef struct RSDT_t {
+    struct ACPISDTHeader_t h;
+    uint32_t PointerToOtherSDT[]; // PointerToOtherSDT[(h.Length - sizeof(h)) / 4];
+} RSDT_t;
+
+struct XSDT_t {
+  struct ACPISDTHeader_t h;
+  uint64_t PointerToOtherSDT[]; // PointerToOtherSDT[(h.Length - sizeof(h)) / 8];
+} XSDT_t;
+
+static inline size_t rsdt_pointer_size(const struct ACPISDTHeader_t *h) {
+    return (h->Length - sizeof(*h)) / 4;
+}
+
+static inline size_t xsdt_pointer_size(const struct ACPISDTHeader_t *h) {
+    return (h->Length - sizeof(*h)) / 8;
+}
+
+typedef struct GenericAddressStructure_t
+{
+  uint8_t AddressSpace;
+  uint8_t BitWidth;
+  uint8_t BitOffset;
+  uint8_t AccessSize;
+  uint64_t Address;
+} GenericAddressStructure_t;
+
+typedef struct FADT_t
+{
+    struct   ACPISDTHeader_t h;
+    uint32_t FirmwareCtrl;
+    uint32_t Dsdt;
+
+    // field used in ACPI 1.0; no longer in use, for compatibility only
+    uint8_t  Reserved;
+
+    uint8_t  PreferredPowerManagementProfile;
+    uint16_t SCI_Interrupt;
+    uint32_t SMI_CommandPort;
+    uint8_t  AcpiEnable;
+    uint8_t  AcpiDisable;
+    uint8_t  S4BIOS_REQ;
+    uint8_t  PSTATE_Control;
+    uint32_t PM1aEventBlock;
+    uint32_t PM1bEventBlock;
+    uint32_t PM1aControlBlock;
+    uint32_t PM1bControlBlock;
+    uint32_t PM2ControlBlock;
+    uint32_t PMTimerBlock;
+    uint32_t GPE0Block;
+    uint32_t GPE1Block;
+    uint8_t  PM1EventLength;
+    uint8_t  PM1ControlLength;
+    uint8_t  PM2ControlLength;
+    uint8_t  PMTimerLength;
+    uint8_t  GPE0Length;
+    uint8_t  GPE1Length;
+    uint8_t  GPE1Base;
+    uint8_t  CStateControl;
+    uint16_t WorstC2Latency;
+    uint16_t WorstC3Latency;
+    uint16_t FlushSize;
+    uint16_t FlushStride;
+    uint8_t  DutyOffset;
+    uint8_t  DutyWidth;
+    uint8_t  DayAlarm;
+    uint8_t  MonthAlarm;
+    uint8_t  Century;
+
+    // reserved in ACPI 1.0; used since ACPI 2.0+
+    uint16_t BootArchitectureFlags;
+
+    uint8_t  Reserved2;
+    uint32_t Flags;
+
+    // 12 byte structure; see below for details
+    GenericAddressStructure_t ResetReg;
+
+    uint8_t  ResetValue;
+    uint8_t  Reserved3[3];
+  
+    // 64bit pointers - Available on ACPI 2.0+
+    uint64_t                X_FirmwareControl;
+    uint64_t                X_Dsdt;
+
+    GenericAddressStructure_t X_PM1aEventBlock;
+    GenericAddressStructure_t X_PM1bEventBlock;
+    GenericAddressStructure_t X_PM1aControlBlock;
+    GenericAddressStructure_t X_PM1bControlBlock;
+    GenericAddressStructure_t X_PM2ControlBlock;
+    GenericAddressStructure_t X_PMTimerBlock;
+    GenericAddressStructure_t X_GPE0Block;
+    GenericAddressStructure_t X_GPE1Block;
+} FADT_t;
+
+#endif // ACPI_H
--- bare-bones/src/kernel.c
+++ bare-bones/src/kernel.c
@@ -8,6 +8,7 @@
 #include "ps2.h"
 #include "multiboot2.h"
 #include "fbout.h"
+#include "acpi.h"
 
 
 #if !defined(__i386__)
@@ -16,11 +17,24 @@
 
 #define FOREACHTAG(tag, addr) for(struct multiboot_tag *tag = (struct multiboot_tag *)((uint8_t *)(addr) + 8); tag->type != MULTIBOOT_TAG_TYPE_END; tag = (struct multiboot_tag *)((uint8_t *)(tag) + ((tag->size + 7) & ~7)))
 
-void kernel_main(multiboot_info_t* mbd, uint32_t magic) {
+FADT_t *findFACP(void *RootSDT) {
+    RSDT_t *rsdt = (RSDT_t *) RootSDT;
+    int entries = (rsdt->h.Length - sizeof(rsdt->h)) / 4;
+
+    for (int i = 0; i < entries; i++)
+    {
+        ACPISDTHeader_t *h = (ACPISDTHeader_t *) rsdt->PointerToOtherSDT[i];
+        if (!strcmp(h->Signature, "FACP"))
+            return (void *) h;
+    }
+
+    // No FACP found
+    return NULL;
+}
+
 void kernel_main(uint32_t multiboot_addr, uint32_t magic) {
     vga_init();
 
-    if(magic != MULTIBOOT_BOOTLOADER_MAGIC) {
     unsigned size = *(unsigned *) multiboot_addr;
 
     if(magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
@@ -32,6 +46,68 @@ void kernel_main(uint32_t multiboot_addr, uint32_t magic) {
         vga_printf("Unaligned mbi: 0x%x\n", multiboot_addr);
         while(true);
     }
+    
+    framebuffer fb;
+    XSDP_t *rsdp = NULL;
+    FADT_t* fadt = NULL;
+    
+    bool fb_found = false;
+    FOREACHTAG(tag, multiboot_addr) {
+        switch(tag->type) {
+            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
+                struct multiboot_tag_framebuffer_common *fbtag = (struct multiboot_tag_framebuffer_common *) tag;
+                if (fbtag->framebuffer_type != MULTIBOOT_FRAMEBUFFER_TYPE_RGB) {
+                    vga_printf("Unsupported framebuffer type: %d\n", fbtag->framebuffer_type);
+                    while (1);
+                }
+            
+                fb.width = fbtag->framebuffer_width;
+                fb.height = fbtag->framebuffer_height;
+                fb.bpp = fbtag->framebuffer_bpp;
+                fb.pitch = fbtag->framebuffer_pitch;
+                fb.addr = (uint32_t *)(uintptr_t)fbtag->framebuffer_addr;
+
+                fb_printf(&fb, "Framebuffer found!\n");
+                fb_printf(&fb, "Framebuffer width: %d\n", fb.width);
+                fb_printf(&fb, "Framebuffer height: %d\n", fb.height);
+                fb_printf(&fb, "Framebuffer bpp: %d\n", fb.bpp);
+                fb_printf(&fb, "Framebuffer pitch: %d\n", fb.pitch);
+                fb_printf(&fb, "Framebuffer address: 0x%x\n", fb.addr);
+                
+                fb_found = true;
+                break;
+            case MULTIBOOT_TAG_TYPE_ACPI_NEW:
+                struct multiboot_tag_new_acpi *new_acpi = (struct multiboot_tag_new_acpi *) tag;
+                rsdp = (XSDP_t *) new_acpi->rsdp;
+                break;
+            case MULTIBOOT_TAG_TYPE_ACPI_OLD:
+                struct multiboot_tag_old_acpi *old_acpi = (struct multiboot_tag_old_acpi *) tag;
+                rsdp = (XSDP_t *) old_acpi->rsdp;
+                break;
+        }
+    }
+
+    if(!rsdp) fb_printf(&fb, "ACPI RSDP not found\n");
+    else {
+        void *root_sdt = (void *)(uintptr_t) (rsdp->xsdt_address ? rsdp->xsdt_address : rsdp->rsdt_address);
+        fadt = (FADT_t *) findFACP(root_sdt);
+        fb_printf(&fb, "ACPI RSDP revision: %s\n", rsdp->revision == 0 ? "1.0" : ">1.0");
+        if(!fadt) fb_printf(&fb, "FADT not found");
+        else {
+            fb_printf(&fb, "FADT Revision: %d\n", fadt->h.Revision);
+            fb_printf(&fb, "FirmwareCtrl: 0x%x\n", fadt->FirmwareCtrl);
+            fb_printf(&fb, "DSDT Address: 0x%x\n", fadt->Dsdt);
+            fb_printf(&fb, "Preferred PM Profile: %d\n", fadt->PreferredPowerManagementProfile);
+            fb_printf(&fb, "SCI Interrupt: %d\n", fadt->SCI_Interrupt);
+            fb_printf(&fb, "SMI Command Port: 0x%x\n", fadt->SMI_CommandPort);
+            fb_printf(&fb, "ACPI Enable: 0x%x\n", fadt->AcpiEnable);
+            fb_printf(&fb, "ACPI Disable: 0x%x\n", fadt->AcpiDisable);
+            fb_printf(&fb, "Reset Register Address: 0x%x\n", fadt->ResetReg.Address);
+            fb_printf(&fb, "Reset Value: 0x%x\n", fadt->ResetValue);
+        }
+    }
+    
+    if(fb_found) {
         // GRAPHICS MODE
         uint8_t res = ps2_keyboard_init();
         if(res != 0) {
