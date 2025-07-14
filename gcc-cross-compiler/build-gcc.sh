3325ca642ceb7b594ef6438834cef08572be340d
Ariel Roque <arielroque@protonmail.com>
2025-07-14 03:25:24 +0200

    Build gcc with more threads

--- gcc-cross-compiler/build-gcc.sh
+++ gcc-cross-compiler/build-gcc.sh
@@ -7,9 +7,9 @@ which -- $TARGET-as || echo $TARGET-as is not in the PATH
 mkdir build-gcc
 cd build-gcc
 ../gcc/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --disable-hosted-libstdcxx
-make all-gcc
-make all-target-libgcc
-make all-target-libstdc++-v3
-make install-gcc
-make install-target-libgcc
-make install-target-libstdc++-v3
+make -j$(nproc) all-gcc
+make -j$(nproc) all-target-libgcc
+make -j$(nproc) all-target-libstdc++-v3
+make -j$(nproc) install-gcc
+make -j$(nproc) install-target-libgcc
+make -j$(nproc) install-target-libstdc++-v3
