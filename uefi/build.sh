37d4e6400b7001affe32ea35a2956615cc1f3d39
Ariel Roque <arielroque@protonmail.com>
2025-08-15 19:57:41 +0200

    Create legacy branch

--- uefi/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-COMPILER=gcc
-
-$COMPILER -o build -Wall -Wextra build.c
-./build compiler:$COMPILER run
--- uefi/src/efi.c
+++
@@ -1,9 +0,0 @@
-#include "efi_common.h"
-#include "efi_boot_services.h"
-#include "efi_runtime_services.h"
-
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-	SystemTable->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
-
-	return EFI_SUCCESS;
-}
