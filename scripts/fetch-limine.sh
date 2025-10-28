46e0e6e6e9e6e5de0856485ec527fb5261a16e78
Ariel Roque <arielroque@protonmail.com>
2025-10-28 17:54:56 +0100

    Remove unused scripts

--- scripts/fetch-limine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ ! -d "limine" ]; then
-    git clone https://codeberg.org/Limine/Limine.git limine --branch=v10.x-binary --depth=1
-    make -C limine
-fi
--- scripts/qemu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-qemu-system-x86_64 -cdrom sak.iso -m 512M -vga std
