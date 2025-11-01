085e96347912f4fdb0576560211bb60c43c5103b
Ariel Roque <arielroque@protonmail.com>
2025-11-01 16:52:23 +0100

    Create CR register utility functions

---
+++ include/kernel/cpu/cr.h
@@ -0,0 +1,83 @@
+#ifndef CR_H
+#define CR_H
+
+#include <stdint.h>
+
+#define CR0_PE (1 << 0)
+#define CR0_MP (1 << 1)
+#define CR0_EM (1 << 2)
+#define CR0_TS (1 << 3)
+#define CR0_ET (1 << 4)
+#define CR0_NE (1 << 5)
+#define CR0_WP (1 << 16)
+#define CR0_AM (1 << 18)
+#define CR0_NW (1 << 29)
+#define CR0_CD (1 << 30)
+#define CR0_PG (1 << 31)
+
+#define CR3_PWT (1 << 3)
+#define CR3_PCD (1 << 5)
+#define CR3_PCID (0x00000fffUL)
+#define CR3_PML4_PBA (0xfffff000UL)
+
+#define CR4_VME (1 << 0)
+#define CR4_PVI (1 << 1)
+#define CR4_TSD (1 << 2)
+#define CR4_DE (1 << 3)
+#define CR4_PSE (1 << 4)
+#define CR4_PAE (1 << 5)
+#define CR4_MCE (1 << 6)
+#define CR4_PGE (1 << 7)
+#define CR4_PCE (1 << 8)
+#define CR4_OSFXSR (1 << 9)
+#define CR4_OSXMMEXCPT (1 << 10)
+#define CR4_UMIP (1 << 11)
+#define CR4_LA57 (1 << 12)
+#define CR4_VMXE (1 << 13)
+#define CR4_SMXE (1 << 14)
+#define CR4_FSGSBASE (1 << 16)
+#define CR4_PCIDE (1 << 17)
+#define CR4_OSXSAVE (1 << 18)
+#define CR4_SMEP (1 << 20)
+#define CR4_SMAP (1 << 21)
+#define CR4_PKE (1 << 22)
+#define CR4_CET (1 << 23)
+#define CR4_PKS (1 << 24)
+
+static inline uint64_t read_cr0() {
+    uint64_t val;
+    __asm__ volatile("mov %%cr0, %0" : "=r"(val));
+    return val;
+}
+
+static inline void write_cr0(uint64_t val) {
+    __asm__ volatile("mov %0, %%cr0" :: "r"(val) : "memory");
+}
+
+static inline uint64_t read_cr2() {
+    uint64_t val;
+    __asm__ volatile("mov %%cr2, %0" : "=r"(val));
+    return val;
+}
+
+static inline uint64_t read_cr3() {
+    uint64_t val;
+    __asm__ volatile("mov %%cr3, %0" : "=r"(val));
+    return val;
+}
+
+static inline void write_cr3(uint64_t val) {
+    __asm__ volatile("mov %0, %%cr3" :: "r"(val) : "memory");
+}
+
+static inline uint64_t read_cr4() {
+    uint64_t val;
+    __asm__ volatile("mov %%cr4, %0" : "=r"(val));
+    return val;
+}
+
+static inline void write_cr4(uint64_t val) {
+    __asm__ volatile("mov %0, %%cr4" :: "r"(val) : "memory");
+}
+
+#endif // CR_H
